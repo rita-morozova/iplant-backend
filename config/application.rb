0157cd3fe37ba7e52f045230c71a3bf719a8851d
MullaN <admullan@gmail.com>
2020-11-06 13:41:48 -0800

    fixed cors

--- config/application.rb
+++ config/application.rb
@@ -33,8 +33,8 @@ module IplantBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    
-    config.middleware.insert_before 0, "Rack::Cors" do
+
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource(
@@ -43,5 +43,6 @@ module IplantBackend
           methods: [:get, :patch, :put, :delete, :post, :options]
           )
       end
+    end
   end
 end
